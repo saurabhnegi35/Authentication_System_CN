c2c92b5c32891395514a5ca9298e03a56cff6589
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-03-24 09:27:12 +0530

    Final Commit

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+// mongoose.connect("mongodb://localhost/AuthenticationSystem");
+
+mongoose.connect(process.env.MONGODB_URL);
+
+//accuire the connectiontion
+const db = mongoose.connection;
+
+//error
+db.on("error", console.error.bind(console, "error in connecting to db"));
+
+//up and runnning
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
+});
